d0a2afafc4e2abe585d913603fca8d388b89e272
niaz151 <niaz151@gmail.com>
2020-05-29 17:14:43 -0400

    Created Checkout UI

--- src/components/ShoppingCartLanding.js
+++ src/components/ShoppingCartLanding.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/ShoppingCartLanding.css';
 import {useDispatch, useSelector} from 'react-redux';
 import { render } from '@testing-library/react';
-import ShoppingCartRow from './ShoppingCartRow';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const ShoppingCartLanding = () => {
   
@@ -11,20 +11,33 @@ const ShoppingCartLanding = () => {
   const shopping_cart = useSelector(state => state['shopping_cart'].items);
   console.log(shopping_cart)
 
-  var output = [];
-  for(var i = 0; i < shopping_cart.length; i++){
-    output.push(
-      <ShoppingCartRow title={shopping_cart[i].title} img={shopping_cart[i].img}  price={shopping_cart[i].price} />
-    )
+  var price = 0;
+  for(var i = 0; i < shopping_cart.length; i ++){
+    price += shopping_cart[i].price;
   }
+  price = price.toFixed(2);
 
   return(
     <div className='shoppingcart-landing'>
-      <div class='shoppingcart-row-wrap'>
-        {output}
-      </div>
       <div className='shoppingcart-menu'>
-        
+        <div className='shoppingcart-menu-icon'>
+          <ShoppingCartIcon fontSize={"large"} />
+        </div>
+        <div className='shoppingcart-menu-items'>
+          <div className='shoppingcart-menu-item'>
+
+          </div>
+          <div className='shoppingcart-menu-item'>
+            
+          </div>
+          <div className='shoppingcart-menu-item'>
+            
+          </div>
+        </div>
+        <div className='shoppingcart-menu-pricing'>
+          <span> Order Total: </span>
+          <span> ${price} </span>
+        </div>
       </div>
     </div>
   )
